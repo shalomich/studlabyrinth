6a1ca0e26eae7c4fcf5c3aa63c32d2c75e19123f
Gevorg17 <ggevorgyan017@gmail.com>
2021-04-15 17:12:31 +0700

    xml navigation added

--- app/src/main/java/com/example/studlabyrinth/MapsActivity.java
+++ app/src/main/java/com/example/studlabyrinth/MapsActivity.java
@@ -2,26 +2,26 @@ package com.example.studlabyrinth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MapsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainpage);
+        setContentView(R.layout.mapspage);
     }
 
-    public void onClickMaps(View view) {
-        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+    public void onClickMain2(View view) {
+        Intent intent = new Intent(MapsActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
     public void onClickHistory(View view) {
-        Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+        Intent intent = new Intent(MapsActivity.this, HistoryActivity.class);
         startActivity(intent);
     }
 }

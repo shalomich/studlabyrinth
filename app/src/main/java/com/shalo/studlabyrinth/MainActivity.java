f5c24e51e9b0d5a89772cdaf457b5ec544e34553
Gevorg17 <45511485+Gevorg17@users.noreply.github.com>
2021-05-14 21:25:18 +0700

    xml data updated

--- app/src/main/java/com/shalo/studlabyrinth/MainActivity.java
+++ app/src/main/java/com/shalo/studlabyrinth/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.shalo.studlabyrinth.MapsActivity;
 import com.shalo.studlabyrinth.R;
+import com.shalo.studlabyrinth.models.Route;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickMaps(View view) {
+        Route routeToSend = new Route(((Spinner) findViewById(R.id.MapName)).getSelectedItem().toString(),
+                                      ((Spinner) findViewById(R.id.BeginningPointName)).getSelectedItem().toString(),
+                                      ((Spinner) findViewById(R.id.endPointName)).getSelectedItem().toString());
+
+        System.out.println(routeToSend);
         Intent intent = new Intent(MainActivity.this, MapsActivity.class);
         startActivity(intent);
     }
